69c708c762637d40f6aec5b5e86b84bea8062ac6
SHAM <shamarknibbs@gmail.com>
2025-11-12 11:10:38 -0500

    Make app responsive for mobile devices

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,9 +25,14 @@ export default function RootLayout({
   return (
 <html lang="en">
   <head>
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, viewport-fit=cover"
+    />
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
     <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@400;600;700&display=swap" rel="stylesheet" />
+    
   </head>
   <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
